3c8c98de5b0f629096bc24f7564837712d691c30
sts <stsdev2@gmail.com>
2025-06-14 18:54:13 +0900

    tmp

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // GitHub 저장소 이름으로 변경
+  base: '/stopthesteel', // GitHub 저장소 이름으로 변경
   build: {
     outDir: 'docs',
   },
